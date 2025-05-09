1f64c793096d23a65d2246fc74e5845284cceb2e
SquilliamH <williamlh3rd@gmail.com>
2025-05-09 14:14:44 -0700

    working pent_static

--- tests/System Tests/pent_static.py
+++ tests/System Tests/pent_static.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
     core.CANHelper.init("can0")
     can0 = can.ThreadSafeBus(channel='can0', bustype='socketcan')
 
+    m_static = CanMotor(can0, motor_id=7, gear_ratio=1)
     m_dynamic = CanMotor(can0, motor_id=0, gear_ratio=1)
 
-    motors = [m_dynamic]
+    motors = [m_static, m_dynamic]  # Fix: add m_static to list
     motor_listener = MotorListener(motor_list=motors)
 
     notifier = can.Notifier(can0, [motor_listener])
@@ -29,6 +30,11 @@ if __name__ == "__main__":
     time.sleep(1)
     input("Continue")
 
+    m_static.read_status_once()
+    m_static.read_multiturn_once()
+    m_static.read_motor_state_once()
+    m_static.datadump()
+
     m_dynamic.read_status_once()
     m_dynamic.read_multiturn_once()
     m_dynamic.read_motor_state_once()
@@ -36,14 +42,14 @@ if __name__ == "__main__":
 
     t = float(sys.argv[1])
 
+    m_static.set_control_mode("torque", -t)
     m_dynamic.set_control_mode("torque", -t)
-
-    #m_static.control()
-    m_dynamic.control()
 
     try:
         while True:
             print("=============================dynamic=============================")
+            m_static.control()
+            m_dynamic.control()
             m_dynamic.read_status_once()
             m_dynamic.read_multiturn_once()
             m_dynamic.read_motor_state_once()
